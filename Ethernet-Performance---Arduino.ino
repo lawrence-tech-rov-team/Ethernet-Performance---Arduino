317f3a159e663136e9501824991582690bf826c5
zcareyltu <zcarey@ltu.edu>
2019-11-14 00:14:48 -0500

    Sending and receiving

--- Ethernet-Performance---Arduino.ino
+++ Ethernet-Performance---Arduino.ino
@@ -10,6 +10,10 @@ static byte gwip[] = { 192,168,178,1 };
 //What to set the Arduino's ethernet mac address to - must be unique on your network
 static byte mymac[] = { 0x70,0x69,0x69,0x2D,0x30,0x31 };
 
+unsigned int recvPort = 6001;
+unsigned int destPort = 6002;
+static byte destIP[] = { 255,255,255,255 }; //Broadcast to all
+
 #define CS 8 //CS pin
 
 byte Ethernet::buffer[500]; // tcp/ip send and receive buffer
@@ -45,12 +49,44 @@ void setup(){
   ether.printIp("GW:  ", ether.gwip);
   ether.printIp("DNS: ", ether.dnsip);
 
-  //Register the udpSerialPrint() method to listen to port 6003
-  ether.udpServerListenOnPort(&udpSerialPrint, 6003);
+  //Register the udpSerialPrint() method to listen to port the receive port.
+  ether.udpServerListenOnPort(&udpSerialPrint, recvPort);
 }
+
+uint16_t count = 0;
+uint32_t timer = 0;
+uint32_t currentTime;
 
 void loop(){
   //this must be called for ethercard functions to work.
   ether.packetLoop(ether.packetReceive());
+
+  currentTime = millis();
+  if((currentTime - timer) >= 1000){
+    timer += 1000;
+    Serial.print("--> ");
+    Serial.println(count);
+    sendCountMsg(count++);
+  }
+}
+
+//Size of temp buffer to send message
+#define MSG_SIZE 6 
+
+//Sends the number 'num' over ethernet using UDP
+void sendCountMsg(uint16_t num){
+  static char msg[MSG_SIZE]; //Message buffer
+
+  char* msgStart = &msg[MSG_SIZE]; //The start of the message. Moves as more digits are added.
+  uint8_t len = 0; //The current length of the message, or the number of digits.
+
+  do{
+    *(--msgStart) = '0' + (num % 10); //Move the message pointer and set the new character to the right most digit in the number
+    len++; //Increase the length since a new digit was added
+    num /= 10; //Shift the number to the right by one, popping the right most digit from the number.
+  }while((num > 0) && (len < MSG_SIZE)); //Repeat until no digits are remaining or the buffer is full.
+
+  //Send the message using our handy-dandy library
+  ether.sendUdp(msgStart, len, recvPort, destIP, destPort);
 }
 
